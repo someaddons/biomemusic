a552769cf14bf30b1f618fecc902facca47a5a6e
someaddons <someaddons@github.com>
2023-08-05 23:43:04 +0200

    1.8: Add cupboard config Add datapack possibility for custom sounds for nether/end/game music

--- src/main/java/com/biomemusic/mixin/SoundManagerWarnMixin.java
+++ src/main/java/com/biomemusic/mixin/SoundManagerWarnMixin.java
@@ -4,7 +4,7 @@ import com.biomemusic.BiomeMusic;
 import net.minecraft.client.resources.sounds.Sound;
 import net.minecraft.client.sounds.SoundManager;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceProvider;
+import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,10 +15,7 @@ public class SoundManagerWarnMixin
 {
     @Inject(method = "validateSoundResource", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", remap = false), cancellable = true)
     private static void biomeMusic$ignorewarn(
-      final Sound sound,
-      final ResourceLocation resourceLocation,
-      final ResourceProvider resourceProvider,
-      final CallbackInfoReturnable<Boolean> cir)
+      final Sound sound, final ResourceLocation p_120397_, final ResourceManager p_120398_, final CallbackInfoReturnable<Boolean> cir)
     {
         if (sound.getPath().getNamespace().equals(BiomeMusic.MODID))
         {
