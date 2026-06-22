76f3bafc43d2d26ad2a60445e09af80f732287e0
someaddons <someaddons@github.com>
2026-06-22 21:04:22 +0200

    3.9: Fix randomness sometimes playing the same music too frequently in a row Improved detection for environment changes to allow the smart music setting to more consistently fade out music which should not play anymore

--- src/main/java/com/biomemusic/MusicChoice.java
+++ src/main/java/com/biomemusic/MusicChoice.java
@@ -21,17 +21,20 @@ import static com.biomemusic.AdditionalMusic.WATER_ADDITIONAL;
 public class MusicChoice
 {
     private final static Random musicRandom = new Random();
-    public static        long   randomSeed  = BiomeMusic.rand.nextLong();
 
-    private static Music               lastChoice          = null;
+    public static       Music      lastChoice            = null;
+    public static final Set<Music> currentPossibleTracks = Collections.newSetFromMap(new IdentityHashMap<>());
+
+    private static boolean lastChoicePlayed = false;
     private static CaveDetectionSystem caveDetectionSystem = new CaveDetectionSystem();
 
     public static void chooseMusic(final CallbackInfoReturnable<Music> cir)
     {
-        caveDetectionSystem.tick(Minecraft.getInstance());
+        final CaveDetectionSystem.CaveState caveState = caveDetectionSystem.tick(Minecraft.getInstance());
         final Player player = Minecraft.getInstance().player;
         if (player == null)
         {
+            currentPossibleTracks.clear();
             if (Minecraft.getInstance().getMusicManager().isPlayingMusic(AdditionalMusic.MENU_ADDITIONAL) || (BiomeMusic.rand.nextInt(5) == 0 && !AdditionalMusic.DISABLED.contains(
                 AdditionalMusic.MENU_ADDITIONAL)
                 && !Minecraft.getInstance().getMusicManager().isPlayingMusic(Musics.MENU)))
@@ -53,7 +56,7 @@ public class MusicChoice
         envChanged |= MusicEnvironment.setEnvironmentFor(MusicEnvironment.OVERWORLD, player.level().dimension() == Level.OVERWORLD);
         envChanged |= MusicEnvironment.setEnvironmentFor(MusicEnvironment.OTHERDIMENSIONS,
             !(player.level().dimension() == Level.END || player.level().dimension() == Level.NETHER || player.level().dimension() == Level.OVERWORLD));
-        envChanged |= MusicEnvironment.setEnvironmentFor(MusicEnvironment.CAVE, caveDetectionSystem.tick(Minecraft.getInstance()).goodForMusic());
+        envChanged |= MusicEnvironment.setEnvironmentFor(MusicEnvironment.CAVE, caveState.goodForMusic());
         envChanged |= MusicEnvironment.setEnvironmentFor(MusicEnvironment.NIGHT,
             player.level().dimensionType().hasSkyLight() && !player.level().dimensionType().hasFixedTime() && (player.level().getDayTime() % 24000) > 12600);
 
@@ -185,26 +188,42 @@ public class MusicChoice
             }
         }
 
+        currentPossibleTracks.clear();
+        currentPossibleTracks.addAll(possibleTracks);
+
         if (possibleTracks.isEmpty())
         {
             Minecraft.getInstance().getMusicManager().nextSongDelay = 2 * 60 * 20;
             return;
         }
 
-        musicRandom.setSeed(randomSeed);
-        Music choice = possibleTracks.get(musicRandom.nextInt(possibleTracks.size()));
-        if (lastChoice != choice)
+        if (lastChoice != null && possibleTracks.contains(lastChoice))
         {
-            newSeed();
-            musicRandom.setSeed(randomSeed);
-            choice = possibleTracks.get(musicRandom.nextInt(possibleTracks.size()));
-            lastChoice = choice;
-        }
-        cir.setReturnValue(choice);
-    }
+            if (Minecraft.getInstance().getMusicManager().isPlayingMusic(lastChoice))
+            {
+                lastChoicePlayed = true;
+            }
 
-    private static void newSeed()
-    {
-        randomSeed = BiomeMusic.rand.nextLong();
+            if (!lastChoicePlayed || Minecraft.getInstance().getMusicManager().isPlayingMusic(lastChoice))
+            {
+                cir.setReturnValue(lastChoice);
+                return;
+            }
+        }
+
+        if (lastChoice != null)
+        {
+            possibleTracks.removeIf(track -> track == lastChoice);
+            if (possibleTracks.isEmpty())
+            {
+                possibleTracks.add(lastChoice);
+            }
+        }
+
+        Music choice = possibleTracks.get(musicRandom.nextInt(possibleTracks.size()));
+        lastChoice = choice;
+        lastChoicePlayed = false;
+
+        cir.setReturnValue(choice);
     }
 }
