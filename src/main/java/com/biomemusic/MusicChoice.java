2144ca20ababbbe7f71c37f4ecf225ca1084fbd2
someaddons <someaddons@github.com>
2026-06-23 20:21:51 +0200

    4.0: Fix game/creative music chosen too often when biome music is not present

--- src/main/java/com/biomemusic/MusicChoice.java
+++ src/main/java/com/biomemusic/MusicChoice.java
@@ -164,7 +164,7 @@ public class MusicChoice
         {
             // Add biome music
             Holder<Biome> holder = player.level().getBiome(player.blockPosition());
-            final Music biomeMusic = (vanillaMusic != Musics.GAME && vanillaMusic != Musics.CREATIVE) ? vanillaMusic : null;
+            final Music biomeMusic = (vanillaMusic.sound() != Musics.GAME.sound() && vanillaMusic.sound() != Musics.CREATIVE.sound()) ? vanillaMusic : null;
             if (biomeMusic != null)
             {
                 possibleTracks.add(biomeMusic);
