53a25486fb305c562dc49fac9113831732cf6dd5
someaddons <someaddons@github.com>
2026-06-23 20:31:50 +0200

    4.1: Fix music variance adding too much weight to some music

--- src/main/java/com/biomemusic/MusicChoice.java
+++ src/main/java/com/biomemusic/MusicChoice.java
@@ -180,8 +180,14 @@ public class MusicChoice
                 {
                     if (holder.is(entry.getKey()))
                     {
-                        possibleTracks.addAll(entry.getValue());
-                        possibleTracks.addAll(entry.getValue());
+                        for (final Music toAdd : entry.getValue())
+                        {
+                            if (!possibleTracks.contains(toAdd))
+                            {
+                                possibleTracks.addAll(entry.getValue());
+                                possibleTracks.addAll(entry.getValue());
+                            }
+                        }
                     }
                 }
 
@@ -189,8 +195,14 @@ public class MusicChoice
                 {
                     if (holder.unwrapKey().isPresent() && holder.unwrapKey().get().identifier().getPath().contains(entry.getKey()))
                     {
-                        possibleTracks.addAll(entry.getValue());
-                        possibleTracks.addAll(entry.getValue());
+                        for (final Music toAdd : entry.getValue())
+                        {
+                            if (!possibleTracks.contains(toAdd))
+                            {
+                                possibleTracks.addAll(entry.getValue());
+                                possibleTracks.addAll(entry.getValue());
+                            }
+                        }
                     }
                 }
             }
